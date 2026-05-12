98b5ab69689ed908b7892876af39ee177a669cd4
Navii <ivanlouis03@gmail.com>
2026-05-13 00:57:29 +0800

    Added idle animations

--- sikap-tala/Levels/alex.gd
+++ sikap-tala/Levels/alex.gd
@@ -2,16 +2,32 @@ extends CharacterBody2D
 
 @export var speed: float = 400.0
 
+var facing := "right"  # track last direction
+
 func _ready() -> void:
 	add_to_group("Player") # make sure alex is in the Player group
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("left", "right")
 
-	if direction:
+	if direction != 0:
 		velocity.x = direction * speed
+
+		# Update facing direction
+		if direction < 0:
+			facing = "left"
+			$AnimatedSprite2D.play("walk_left")
+		else:
+			facing = "right"
+			$AnimatedSprite2D.play("walk_right")
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+
+		# Idle animation based on last facing
+		if facing == "left":
+			$AnimatedSprite2D.play("idle_left")
+		else:
+			$AnimatedSprite2D.play("idle_right")
 
 	move_and_slide()
 
