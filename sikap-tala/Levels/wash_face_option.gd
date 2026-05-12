d8052a089bf1cd3ac66106e17a9a88e53af82721
Navii <ivanlouis03@gmail.com>
2026-05-13 06:50:51 +0800

    Finished the BathroomFailure(Bathroom Success not linked yet) and Forget ending

--- sikap-tala/Levels/wash_face_option.gd
+++ sikap-tala/Levels/wash_face_option.gd
@@ -3,6 +3,8 @@ extends CanvasGroup
 func _ready() -> void:
 	hide() # start hidden
 
-func _on_WashYourFace_pressed() -> void:
-	# Example: change to next scene
-	get_tree().change_scene_to_file("res://NextScene.tscn")
+func show_prompt() -> void:
+	show()
+
+func hide_prompt() -> void:
+	hide()
